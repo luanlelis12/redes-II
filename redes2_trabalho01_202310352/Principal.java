0df6d54149e56f732d3bd97ec4d7247f2e8d235a
Luan Alves Lélis Costa <luanlelis09@gmail.com>
2026-03-16 16:09:47 -0300

    create: criar pasta para início do projeto

---
+++ redes2_trabalho01_202310352/Principal.java
@@ -0,0 +1,38 @@
+/* ***************************************************************
+* Autor............: Luan Alves Lelis Costa
+* Matricula........: 202310352
+* Inicio...........: 16 03 2026
+* Ultima alteracao.:   
+* Nome.............: Principal.java
+* Funcao...........: 
+*************************************************************** */
+
+import util.FxmlRotas;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class Principal extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+
+    // Carrega o arquivo FXML da tela principal
+    FXMLLoader loader = new FXMLLoader(getClass().getResource(FxmlRotas.TELA_INICIAL));
+    Parent root = loader.load();
+
+    // Configura a cena
+    primaryStage.setTitle("");
+    primaryStage.setScene(new Scene(root));
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+  
+}
---
+++ redes2_trabalho01_202310352/util/FxmlRotas.java
@@ -0,0 +1,17 @@
+/* ***************************************************************
+* Autor............: Luan Alves Lelis Costa
+* Matricula........: 202310352
+* Inicio...........: 16 03 2026
+* Ultima alteracao.: 16 03 2026
+* Nome.............: FxmlRota.java
+* Funcao...........: Definir as rotas dos arquivos fxml
+*************************************************************** */
+
+package util;
+
+public class FxmlRotas {
+	
+	public static String TELA_INICIAL = "/view/menuInicial.fxml"; // Rota da tela principal
+	public static String TELA_ROTEAMENTO = "/view/telaDeRoteamento.fxml"; // Rota da tela dos roteadores
+	
+}
