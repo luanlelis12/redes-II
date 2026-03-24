f5e34a6d9cb2d6ccb1c8cc15ba38de03bc692e9e
Luan Alves Lélis Costa <luanlelis09@gmail.com>
2026-03-24 11:43:05 -0300

    feat: adicionar conexoes entre roteadores

--- redes2_trabalho01_202310352/model/Backbone.java
+++ redes2_trabalho01_202310352/model/Backbone.java
@@ -12,6 +12,12 @@ public class Backbone {
   public Backbone() {
   }
 
+  /*
+   * Metodo: carregarArquivo
+   * Funcao: recebe a posicao do roteador e o posiciona na tela
+   * Parametros: posX = posicao horizontal; posY = posicao vertical
+   * Retorno: void
+   */
   public void carregarArquivo(String caminho) {
     try {
       BufferedReader buffRead = new BufferedReader(new FileReader(caminho));
@@ -24,22 +30,61 @@ public class Backbone {
         int valor = Character.getNumericValue(linha.charAt(i));
         quantidadeRoteadores = (quantidadeRoteadores * 10) + valor;
         i++;
-      }
+      } // fim do while
 
-      roteadores.clear();
-      for (int j = 0; j < quantidadeRoteadores; j++) {
-        roteadores.add(new Roteador(j));
-      }
-      
+      gerarRoteadores(quantidadeRoteadores);
+
       System.out.println("Quantidade de roteadores no backbone: " + quantidadeRoteadores);
+
+      while ((linha = buffRead.readLine()) != null) {
+        if (linha.trim().isEmpty())
+          continue;
+
+        String[] partes = linha.split(";");
+
+        if (partes.length == 3) {
+          int idOrigem = Integer.parseInt(partes[0].trim());
+          int idDestino = Integer.parseInt(partes[1].trim());
+          int peso = Integer.parseInt(partes[2].trim());
+
+          Roteador r1 = roteadores.get(idOrigem-1);
+          Roteador r2 = roteadores.get(idDestino-1);
+
+          gerarConexao(r1, r2, peso);
+
+          System.out.println("Conexão criada: " + idOrigem + " <-> " + idDestino + " peso " + peso);
+        } // fim do if
+      } // fim do while
+
       buffRead.close();
     } catch (IOException e) {
       e.printStackTrace();
-    }
-  }
+    } // fim do try-catch
+  } // fim do metodo carregarArquivo
 
-  public void gerarRoteadores() {
-  }
+  /*
+   * Metodo: gerarRoteadores
+   * Funcao: cria os roteadores e os adicionam na rede
+   * Parametros: quantR = quantidade de roteadores a ser criado
+   * Retorno: void
+   */
+  public void gerarRoteadores(int quantR) {
+    roteadores.clear();
+    for (int j = 1; j <= quantR; j++) {
+      roteadores.add(new Roteador(j));
+    } // fim do for
+  } // fim do metodo carregarArquivo
+
+  /*
+   * Metodo: gerarConexao
+   * Funcao:
+   * Parametros:
+   * Retorno:
+   */
+  public void gerarConexao(Roteador r1, Roteador r2, int peso) {
+    r1.addVizinho(r2, peso);
+    r2.addVizinho(r1, peso);
+  } // fim do metodo gerarConexao
 
   public ArrayList<Roteador> getRoteadores() {
     return roteadores;
--- redes2_trabalho01_202310352/model/Roteador.java
+++ redes2_trabalho01_202310352/model/Roteador.java
@@ -6,7 +6,7 @@ import javafx.scene.image.ImageView;
 
 public class Roteador {
   private int idRoteador;
-  private ArrayList<Aresta> conexoes;
+  private ArrayList<Aresta> conexoes = new ArrayList<>();
 
   public Roteador(int idRoteador) {
     this.idRoteador = idRoteador;
@@ -16,4 +16,8 @@ public class Roteador {
     return idRoteador;
   }
 
+  public void addVizinho(Roteador vizinho, int peso) {
+    this.conexoes.add(new Aresta(vizinho, peso));
+  }
+ 
 }
