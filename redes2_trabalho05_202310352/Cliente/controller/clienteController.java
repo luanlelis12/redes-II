7698ea345744eef0eb1603df86d398670970def9
luanlelis12 <luanlelis09@gmail.com>
2026-06-16 22:47:41 -0300

    pequenas adicoes

--- redes2_trabalho05_202310352/Cliente/controller/clienteController.java
+++ redes2_trabalho05_202310352/Cliente/controller/clienteController.java
@@ -8,23 +8,40 @@
 *******************************************************************/
 package controller;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
-
+import javafx.stage.StageStyle;
 import model.Cliente;
 import util.processadorTexto;
 
@@ -40,8 +57,16 @@ public class clienteController implements Initializable {
   private TextField nomeGrupoField;
   @FXML
   private TextArea mensagemField;
+  @FXML
+  private Label conversaSelecionadaLabel;
+  @FXML
+  private VBox vboxGrupos;
+  @FXML
+  private VBox conversaVBox;
 
   private static Cliente cliente;
+  private String grupoSelecionado = null;
+  private ArrayList<String> grupos = new ArrayList<>();
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {
@@ -53,7 +78,7 @@ public class clienteController implements Initializable {
         // Verifica se o usuario NAO esta segurando o Shift
         event.consume();
         if (!event.isShiftDown()) {
-          System.out.println(mensagemField.getText());
+          enviarMensagem();
           mensagemField.clear();
         } // fim do if
       } // fim do if
@@ -90,13 +115,57 @@ public class clienteController implements Initializable {
    * Retorno: void
    */
   public void enviarMensagem() {
+
+    if (grupoSelecionado == null)
+      return;
+
     String mensagem = mensagemField.getText();
     mensagemField.clear();
 
+    HBox balaoDeDialogo = criarBalaoDialogo(mensagem, "",true);
+    conversaVBox.getChildren().add(balaoDeDialogo);
+
     mensagem = processadorTexto.inserirFlagEscape(mensagem);
-    System.out.println("Enviando mensagem: " + mensagem);
-    cliente.enviarMensagem("teste", mensagem);
+    System.out.println("Cliente: enviando mensagem \"" + mensagem + "\"");
+    // cliente.enviarMensagem("teste", mensagem);
   } // fim do metodo enviarMensagem
+
+  public HBox criarBalaoDialogo(String mensagem, String nomeRemetente, boolean enviadaPorMim) {
+
+    VBox balao = new VBox(5);
+
+    balao.setMinWidth(200);
+    balao.setMinHeight(50);
+    balao.setMaxWidth(450);
+
+    if (!enviadaPorMim && nomeRemetente != null && !nomeRemetente.isEmpty()) {
+      Label labelNome = new Label(nomeRemetente);
+      labelNome.setStyle("-fx-font-weight: bold;");
+      labelNome.setFont(new Font("Arial", 12));
+      balao.getChildren().add(labelNome);
+    } // fim do if
+
+    Label textoMsg = new Label(mensagem);
+    textoMsg.setFont(new Font("Arial", 14));
+    textoMsg.setWrapText(true);
+
+    balao.getChildren().add(textoMsg);
+
+    HBox linha = new HBox(balao);
+    linha.setPadding(new Insets(5, 15, 5, 15));
+
+    if (enviadaPorMim) {
+      linha.setAlignment(Pos.CENTER_RIGHT);
+      balao.setStyle(
+          "-fx-background-color: #c6e7f8;");
+    } else {
+      linha.setAlignment(Pos.CENTER_LEFT);
+      balao.setStyle(
+          "-fx-background-color: #FFFFFF;");
+    } // fim do if
+
+    return linha;
+  } // fim do metodo criarBalaoDialogo
 
   /*
    * Metodo: entrarGrupo
@@ -106,21 +175,81 @@ public class clienteController implements Initializable {
    */
   public void entrarGrupo() {
     String nomeGrupo = nomeGrupoField.getText();
-    nomeGrupo = processadorTexto.inserirFlagEscape(nomeGrupo);
+    String nomeGrupoProcessado = processadorTexto.inserirFlagEscape(nomeGrupo);
 
-    if (nomeGrupo == null || nomeGrupo.trim().isEmpty()) {
-      Alert alerta = new Alert(AlertType.WARNING);
+    if (nomeGrupoProcessado == null || nomeGrupoProcessado.trim().isEmpty()) {
+      try {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/alert.fxml"));
+        Parent root = loader.load();
 
-      alerta.setTitle("NOME DE GRUPO VAZIO");
-      alerta.setHeaderText("Nenhum nome foi inserido");
-      alerta.setContentText("Digite o nome do grupo para entrar.");
+        alertController controladorDoAlerta = loader.getController();
 
-      alerta.show();
+        controladorDoAlerta.setDetalhes("Nenhum nome foi inserido", "Digite o nome do grupo para entrar.");
+
+        Stage janelaAlerta = new Stage();
+        janelaAlerta.setScene(new Scene(root));
+        janelaAlerta.initStyle(StageStyle.UNDECORATED);
+        janelaAlerta.initModality(Modality.APPLICATION_MODAL);
+
+        janelaAlerta.show();
+      } catch (IOException e) {
+        System.out.println("Erro ao carregar o alerta!");
+      }
       return;
     } // fim do if
 
-    cliente.entrarGrupo(nomeGrupo);
+    grupos.add(nomeGrupo);
+    adicionarGrupoNaTela(nomeGrupo);
+    // cliente.entrarGrupo(nomeGrupoProcessado);
   } // fim do metodo entrarGrupo
+
+  public void adicionarGrupoNaTela(String nomeDoGrupo) {
+    try {
+      AnchorPane itemConversa = new AnchorPane();
+
+      // Image icon = new Image("");
+      ImageView iconConversa = new ImageView();
+
+      iconConversa.setLayoutX(10);
+      iconConversa.setLayoutY(10);
+      iconConversa.setFitHeight(50);
+      iconConversa.setFitWidth(50);
+
+      Label nomeConversa = new Label(nomeDoGrupo);
+
+      nomeConversa.setLayoutX(70);
+      nomeConversa.setLayoutY(20);
+
+      itemConversa.getChildren().addAll(iconConversa, nomeConversa);
+
+      itemConversa.setOnMouseClicked(null);
+      itemConversa.getStyleClass().add("itemConversa");
+
+      itemConversa.setPrefSize(280, 70);
+      itemConversa.setMinSize(280, 70);
+
+      itemConversa.setOnMouseClicked(event -> {
+        grupoSelecionado = nomeDoGrupo;
+        conversaSelecionadaLabel.setText(grupoSelecionado);
+      });
+
+      vboxGrupos.getChildren().add(itemConversa);
+
+    } catch (Exception e) {
+      System.out.println("Erro ao carregar o visual do grupo!");
+    }
+  } // fim do metodo adicionarGrupoNaTela
+
+  /*
+   * Metodo: selecionarGrupo
+   * Funcao:
+   * Parametros:
+   * Retorno: void
+   */
+  public void selecionarGrupo(ActionEvent event) {
+    String grupo = ((Node) event.getSource()).toString();
+    System.out.println(grupo);
+  } // fim do metodo selecionarGrupo
 
   /*
    * Metodo: fecharAplicacao
--- redes2_trabalho05_202310352/Servidor/model/Servidor.java
+++ redes2_trabalho05_202310352/Servidor/model/Servidor.java
@@ -16,6 +16,10 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Semaphore;
 
 public class Servidor extends Thread {
 
@@ -24,6 +28,9 @@ public class Servidor extends Thread {
 
   private InetAddress ipServidor;
   private DatagramSocket endpointServidor;
+  private static Semaphore mutex = new Semaphore(1);
+
+  private Map<String, ArrayList<Usuario>> grupos = new HashMap<>();
 
   public Servidor() {
     try {
@@ -42,7 +49,7 @@ public class Servidor extends Thread {
       try {
         ServerSocket servidor = new ServerSocket(PORTA_TCP);
         while (true) {
-          System.out.println("O SERVIDOR esta esperando receber alguma conexao...");
+          System.out.println("SERVIDOR - esperando receber alguma conexao...");
           Socket conexao = servidor.accept();
           new Thread(() -> {
             ObjectInputStream entrada;
@@ -50,7 +57,7 @@ public class Servidor extends Thread {
               entrada = new ObjectInputStream(conexao.getInputStream());
               String apduRecebida = (String) entrada.readObject();
               System.out.println("APDU recebida: " + apduRecebida);
-              processarApdu(apduRecebida);
+              processarApdu(apduRecebida, conexao.getInetAddress());
             } catch (IOException | ClassNotFoundException e) {
               System.out.println("ERRO: Nao foi possivel receber a APDU do cliente!");
             }
@@ -66,13 +73,13 @@ public class Servidor extends Thread {
         byte[] dadosEntrada = new byte[1024];
 
         DatagramPacket pacoteRecebido = new DatagramPacket(dadosEntrada, dadosEntrada.length);
-        System.out.println("O SERVIDOR esta esperando receber algum pacote...");
+        System.out.println("SERVIDOR - esperando receber algum pacote...");
         endpointServidor.receive(pacoteRecebido);
 
         String apduRecebida = new String(pacoteRecebido.getData());
         new Thread(() -> {
           System.out.println("APDU recebida: " + apduRecebida);
-          processarApdu(apduRecebida);
+          processarApdu(apduRecebida, pacoteRecebido.getAddress());
         }).start();
       }
     } catch (Exception e) {
@@ -86,22 +93,42 @@ public class Servidor extends Thread {
    * Parametros: apduRecebida = APDU enviada pelo servidor
    * Retorno: void
    */
-  public void processarApdu(String apduRecebida) {
+  public void processarApdu(String apduRecebida, InetAddress ipCliente) {
     String[] partes = apduRecebida.split("~~");
     switch (partes[0]) {
       case "JOIN":
-
+        try {
+          String grupo = partes[1];
+          String nome = partes[2];
+          mutex.acquire();
+          inserirNoGrupo(grupo, nome, ipCliente);
+          mutex.release();
+        } catch (Exception e) {
+          // TODO: handle exception
+        }
         break;
       case "LEAVE":
-
+        try {
+          String grupo = partes[1];
+          String nome = partes[2];
+          mutex.acquire();
+          sairDoGrupo(grupo, nome);
+          mutex.release();
+        } catch (Exception e) {
+          // TODO: handle exception
+        }
         break;
       case "SEND":
-        byte[] dadosEnviados = new byte[1024];
-
-        dadosEnviados = apduRecebida.getBytes();
-
-        // DatagramPacket datagramaEnviado = new DatagramPacket(dadosEnviados, dadosEnviados.length, ipCliente, PORTA_UDP);
-        // endpointServidor.send(datagramaEnviado);
+        try {
+          String grupo = partes[1];
+          String nome = partes[2];
+          String mensagem = partes[3];
+          mutex.acquire();
+          enviarMensagem(mensagem, grupo, nome);
+          mutex.release();
+        } catch (Exception e) {
+          // TODO: handle exception
+        }
         break;
       case "SENDPVT":
 
@@ -111,5 +138,69 @@ public class Servidor extends Thread {
         break;
     }
   } // fim do metodo processarApdu
+
+  private void inserirNoGrupo(String grupo, String nome, InetAddress ipCliente) {
+    Usuario novoUsuario = new Usuario(ipCliente, nome, PORTA_TCP);
+    if (grupos.containsKey(grupo)) {
+      grupos.get(grupo).add(novoUsuario);
+    } else {
+      ArrayList<Usuario> listaUsuario = new ArrayList<>();
+      listaUsuario.add(novoUsuario);
+      grupos.put(grupo, listaUsuario);
+    }
+  }
+
+  private void sairDoGrupo(String nomeGrupo, String nomeUsuario) {
+    ArrayList<Usuario> listaDeUsuarios = grupos.get(nomeGrupo);
+    for (Usuario usuario : listaDeUsuarios) {
+      if (usuario.getNome() == nomeUsuario) {
+        grupos.get(nomeGrupo).remove(usuario);
+        System.out.println("SERVIDOR - Removendo o usuario " + nomeUsuario);
+      }
+    }
+
+  }
+
+  private void enviarMensagem(String mensagem, String grupo, String nome) {
+    // TODO Auto-generated method stub
+    throw new UnsupportedOperationException("Unimplemented method 'enviarMensagem'");
+  }
+
+}
+
+class Usuario {
+  InetAddress ip;
+  String nome;
+  int porta;
+
+  public Usuario(InetAddress ip, String nome, int porta) {
+    this.ip = ip;
+    this.nome = nome;
+    this.porta = porta;
+  }
+
+  public InetAddress getIp() {
+    return ip;
+  }
+
+  public void setIp(InetAddress ip) {
+    this.ip = ip;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public int getPorta() {
+    return porta;
+  }
+
+  public void setPorta(int porta) {
+    this.porta = porta;
+  }
 
 }
